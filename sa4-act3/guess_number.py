11a3f5e8ea199a65ebd2f03b8a81379dd7c7268e
Justin-Martin1 <jlmrtn10@memphis.edu>
2024-03-11 18:20:51 -0500

    Changed code to add guesses

--- sa4-act3/guess_number.py
+++ sa4-act3/guess_number.py
@@ -1,9 +1,31 @@
 number = 10
+max_guesses = 5
+guesses_left = max_guesses
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
 
-if guess == number:
-    print("Congratulations! You guessed the right number.")
-else:
-    print(f"Sorry! The number was {number}.")
+while guesses_left > 0:
+    guess = input(f"What number am I thinking of? You have {guesses_left} guesses remaining.")
+
+    if guess.lower() == 'q':
+        print(f"Quitting the game. The number was {number}. Thanks for playing")
+        break
+
+    try:
+        guess = int(guess)
+    except ValueError:
+        print("Please enter a valid number or 'q' to quit.")
+        continue
+    
+    guesses_left -= 1
+
+    if guess == number:
+        print("Congratulations! You guessed the right number.")
+        break
+    elif guess < number:
+        print("Sorry! Your guess was too low.")
+    else:
+        print("Sorry! Your guess was too high.")
+
+    if guesses_left == 0:
+        print(f"Sorry, you've used all your guesses. The number was {number}.")
